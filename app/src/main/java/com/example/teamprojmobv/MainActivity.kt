8d8048b8b76cf66d00ccfb241b8bb6ad7d3757a1
David Sery <david.sery.sk@gmail.com>
2020-11-18 15:06:49 +0100

    Pri spusteni aplikacie sa spusti video vo fragmente. Chcem dokoncit navigaciu aby sa video zobrazilo az po kliknuti na button vo vlastnom okne no teraz nestiham.

--- app/src/main/java/com/example/teamprojmobv/MainActivity.kt
+++ app/src/main/java/com/example/teamprojmobv/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.teamprojmobv
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.FragmentActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
+
+    private val manager = supportFragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val change = findViewById<Button>(R.id.show_video_view)
+
+        change.setOnClickListener { showVideoFragment() }
+    }
+
+    private fun showVideoFragment(){
+        val transaction = manager.beginTransaction()
+        val fragment = VideoFragment()
+        transaction.replace(R.id.video_fragment, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }

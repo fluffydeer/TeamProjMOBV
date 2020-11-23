7306807d74d491dbcb4fdd6fbe31ea2cfb45815d
Natalia <natiklementova@gmail.com>
2020-11-23 20:11:41 +0100

    added packages for MVVM

--- app/src/main/java/com/example/teamprojmobv/Views/CameraFragment.kt
+++ app/src/main/java/com/example/teamprojmobv/Views/CameraFragment.kt
@@ -1,4 +1,4 @@
-package com.example.teamprojmobv
+package com.example.teamprojmobv.Views
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.example.teamprojmobv.R
 import kotlinx.android.synthetic.main.fragment_camera.*
 
 
--- app/src/main/java/com/example/teamprojmobv/Views/VideoViewerFragment.kt
+++ app/src/main/java/com/example/teamprojmobv/Views/VideoViewerFragment.kt
@@ -1,17 +1,18 @@
-package com.example.teamprojmobv
+package com.example.teamprojmobv.Views
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.teamprojmobv.R
 
 
-class ProfileFragment : Fragment() {
+class VideoViewerFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        return inflater.inflate(R.layout.fragment_video_viewer, container, false)
     }
 }
